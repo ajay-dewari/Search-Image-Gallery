bb76e975f7f523c0612639e47a2ad3478d6893c5
ajaydewari <ajdewari@gmail.com>
2021-05-28 23:31:59 +0530

    search menu added to the gallery fragment menu, with search image icon and overrided the onCreateOptionsMenu to innitilize the search

--- app/src/main/java/me/ajay/imagegallery/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/me/ajay/imagegallery/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package me.ajay.imagegallery.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import me.ajay.imagegallery.R
 import me.ajay.imagegallery.databinding.FragmentGalleryBinding
@@ -11,5 +14,13 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentGalleryBinding.bind(view)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.gallery_search_menu, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
     }
 }
