d1c24c24d572939597645d38c499a85e496b03e7
ajaydewari <ajdewari@gmail.com>
2021-05-29 00:11:47 +0530

    scope corrected for the appModule

--- app/src/main/java/me/ajay/imagegallery/di/AppModule.kt
+++ app/src/main/java/me/ajay/imagegallery/di/AppModule.kt
@@ -1,8 +1,11 @@
 package me.ajay.imagegallery.di
 
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
 }
