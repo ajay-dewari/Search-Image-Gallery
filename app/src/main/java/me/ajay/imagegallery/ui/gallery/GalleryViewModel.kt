c6ec981d7223924ad0d2e28cbf75571eab1cbaba
ajaydewari <ajdewari@gmail.com>
2021-05-29 00:43:23 +0530

    properties added to GalleryViewModel for the ImageSearchRepository for api call and SavedStateHandle added to overcome process death

--- app/src/main/java/me/ajay/imagegallery/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/me/ajay/imagegallery/ui/gallery/GalleryViewModel.kt
@@ -1,17 +1,21 @@
 package me.ajay.imagegallery.ui.gallery
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import me.ajay.imagegallery.data.ImageSearchRepository
 import javax.inject.Inject
 
 const val EMPTY_QUERY: String = ""
 
 @HiltViewModel
-class GalleryViewModel @Inject constructor(): ViewModel() {
-
+class GalleryViewModel @Inject constructor(
+    private val repository: ImageSearchRepository,
+    state: SavedStateHandle
+) : ViewModel() {
     private val searchQuery = MutableStateFlow(EMPTY_QUERY);
-
 
     fun setSearchQuery(query: String) {
         searchQuery.value = query
