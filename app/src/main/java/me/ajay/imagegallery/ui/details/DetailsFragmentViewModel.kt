6c64c162f06391ed4f8edb1e7e8c6f0cee9ffd94
ajaydewari <ajdewari@gmail.com>
2021-05-29 11:10:15 +0530

    getHostName method optimised by using the URL class to get the host name

--- app/src/main/java/me/ajay/imagegallery/ui/details/DetailsFragmentViewModel.kt
+++ app/src/main/java/me/ajay/imagegallery/ui/details/DetailsFragmentViewModel.kt
@@ -4,13 +4,14 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import me.ajay.imagegallery.data.GalleryImage
+import java.net.URL
 import javax.inject.Inject
+
 
 @HiltViewModel
 class DetailsFragmentViewModel @Inject constructor(state: SavedStateHandle) :
     ViewModel() {
     val imageItem = state.get<GalleryImage>("image")
 
-    fun getHostName(url: String): String =
-        url.substring(url.indexOf("//")+2, url.indexOf(".com"))
+    fun getHostName(url: String): String = URL(url).host
 }
