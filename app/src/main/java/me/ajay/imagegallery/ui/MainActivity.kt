d63cf7f6b3241be4736d3c1c7b0e032cfb8d6b07
ajaydewari <ajdewari@gmail.com>
2021-05-29 13:43:59 +0530

    BroadcastReceiver added to liste the connectivity changes

--- app/src/main/java/me/ajay/imagegallery/ui/MainActivity.kt
+++ app/src/main/java/me/ajay/imagegallery/ui/MainActivity.kt
@@ -1,5 +1,10 @@
 package me.ajay.imagegallery.ui
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
@@ -9,21 +14,35 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import me.ajay.imagegallery.R
 
-private lateinit var navController: NavController
+lateinit var navController: NavController
+private lateinit var internetBroadcastReceiver : BroadcastReceiver
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.findNavController()
         setupActionBarWithNavController(navController)
+
+        internetBroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(contxt: Context, intent: Intent?) {
+
+            }
+        }
+        this.registerReceiver(internetBroadcastReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.unregisterReceiver(internetBroadcastReceiver)
+    }
+
 }
