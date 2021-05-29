97a7228d26b06d15a45a3454a0e7857eddd0e570
ajaydewari <ajdewari@gmail.com>
2021-05-29 13:31:22 +0530

    navController iss setup to ManinActivityActionbar

--- app/src/main/java/me/ajay/imagegallery/ui/MainActivity.kt
+++ app/src/main/java/me/ajay/imagegallery/ui/MainActivity.kt
@@ -2,13 +2,28 @@ package me.ajay.imagegallery.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import me.ajay.imagegallery.R
+
+private lateinit var navController: NavController
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
