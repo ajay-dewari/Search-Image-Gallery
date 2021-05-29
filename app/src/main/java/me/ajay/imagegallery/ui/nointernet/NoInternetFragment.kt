aa97de0e7cad5c4aae9fcede8b55d2bc2bd20d41
ajaydewari <ajdewari@gmail.com>
2021-05-29 13:57:30 +0530

    NoInternetFragment class created inflating the fragment_no_internet

---
+++ app/src/main/java/me/ajay/imagegallery/ui/nointernet/NoInternetFragment.kt
@@ -0,0 +1,6 @@
+package me.ajay.imagegallery.ui.nointernet
+
+import androidx.fragment.app.Fragment
+import me.ajay.imagegallery.R
+
+class NoInternetFragment : Fragment(R.layout.fragment_no_internet)
